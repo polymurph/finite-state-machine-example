7fe491b63ff6d9f6ebbb8d0dca2e4f03c5bed0bb
polymurph <eddyed.k@gmail.com>
2020-01-20 17:34:51 +0100

    added Statees in the ctrl.h

--- garagedoor_ctrl.h
+++ garagedoor_ctrl.h
@@ -1,14 +1,22 @@
 #ifndef _GARAGEDOOR_CTRL_H_
 #define _GARAGEDOOR_CTRL_H_
 
-
-
-
 class Garagedoor_ctrl:
 {
 public:
+
   Garagedoor_ctrl()
 private:
+  enum State{
+    openingState,
+    openState,
+    closingState,
+    closedState,
+    stopState
+  };
+
+  // holds the current state of the FSM
+  State currentState;
 
 }
 
